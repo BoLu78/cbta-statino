b61b61d5c1390365169084131eb5d6fe238b83eb
LUCA <bortolotluca@gmail.com>
2026-02-11 12:50:05 +0500

    v11.1

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
 /* =========================================================
    CBTA Statino — Service Worker (OFFLINE + GITHUB PAGES)
-   VERSION: v12feb26-8.7
+   VERSION: v12feb26-8.8
    ========================================================= */
 
-const CACHE_NAME = "cbta-statino-v12feb26-8.7";
+const CACHE_NAME = "cbta-statino-v12feb26-8.8";
 const STATIC_CACHE_URLS = [
   "./",
   "./index.html",
